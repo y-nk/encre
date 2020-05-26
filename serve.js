ea461340dbdc44f09763540eb21be327afb7e4cd
Julien Barbay <julien.barbay@gmail.com>
2020-05-26 15:50:09 +0900

    add manual support of mimetype

--- serve.js
+++ serve.js
@@ -1,9 +1,23 @@
 const render = require('./render')
 
+const mimetypes = {
+  '.css': 'text/css',
+  '.jpg': 'image/jpeg',
+  '.png': 'image/png',
+  '.gif': 'image/gif',
+  '.otf': 'text/plain',
+}
+
 module.exports = options => async (req, res) => {
   const { path } = req
 
   const content = await render(path, options)
+
+  const extension = Object.keys(mimetypes)
+    .find(extension => path.endsWith(extension))
+    
+  if (content && extension)
+    res.header('Content-Type', mimetypes[extension])
 
   if (content)
     res.status(200).end(content)
