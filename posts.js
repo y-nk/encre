128174d04d7396fb3af76cfe81e84fb1d159213b
Julien Barbay <julien.barbay@gmail.com>
2020-05-26 15:47:26 +0900

    include markdown content in "get all posts"

--- posts.js
+++ posts.js
@@ -6,9 +6,7 @@ const post = require('./post')
 module.exports = async () => {
   const paths = await glob(`${process.cwd()}/posts/**/*.md`)
 
-  const posts = await Promise.all(
+  return await Promise.all(
     paths.map(path => post(path))
   )
-  
-  return posts.map(({ meta }) => meta)
 }
