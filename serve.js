54c194425a37171b8a6ee8e7a9d6860aa3e3034d
Julien Barbay <julien.barbay@gmail.com>
2020-05-29 03:24:26 +0900

    adding more mimetypes

--- serve.js
+++ serve.js
@@ -1,11 +1,19 @@
 const render = require('./render')
 
 const mimetypes = {
+  '.js': 'application/javascript',
   '.css': 'text/css',
-  '.jpg': 'image/jpeg',
-  '.png': 'image/png',
   '.gif': 'image/gif',
+  '.jpg': 'image/jpeg',
+  '.mp4': 'video/mpeg',
   '.otf': 'text/plain',
+  '.pdf': 'application/pdf',
+  '.png': 'image/png',
+  '.svg': 'image/svg+xml',
+  '.html': 'text/html',
+  '.json': 'application/json',
+  '.webm': 'video/webm',
+  '.webp': 'image/webp',
 }
 
 module.exports = options => async (req, res) => {
